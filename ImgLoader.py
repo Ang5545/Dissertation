362776e82fab1669911208e7323d69990f12c2ef
ange <Angel.a.a@mail.ru>
2017-07-04 19:45:47 +0300

    Initial commit

---
+++ ImgLoader.py
@@ -0,0 +1,58 @@
+import configparser
+import cv2
+from glob import glob
+import configparser
+from os.path import join
+
+
+def __getImgPath__(configFile, parName):
+    #  -- read parameters from config --
+    config = configparser.ConfigParser()
+    config.read(configFile)
+    try:
+        def configSectionMap(section):
+            dict1 = {}
+            options = config.options(section)
+            for option in options:
+                try:
+                    dict1[option] = config.get(section, option)
+                    if dict1[option] == -1:
+                        print("skip: %s" % option)
+                except:
+                    print("exception on %s!" % option)
+                    dict1[option] = None
+            return dict1
+
+        if (configSectionMap("default")[parName]) :
+            return configSectionMap("default")[parName]
+        else:
+            return input('Input image dir path: ')
+
+    # -- read parameters from input --
+    except KeyError:
+        return input('Input image dir path: ')
+    except configparser.NoSectionError:
+        return input('Input image dir path: ')
+
+
+def getImages(parName):
+    imageDirPath = __getImgPath__('config.ini', parName)
+    print('imageDirPath = ' + imageDirPath)
+
+    types = ('*.jpeg', '*.JPG', '*.gif', '*.png', '*.jpg', '*.bmp')
+    filesPaths = []
+
+    for extension in types:
+        filesPaths.extend(glob(join(imageDirPath, extension)))
+
+    imgs = []
+    for path in filesPaths:
+        # img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(path, 3)
+        imgs.append(img)
+
+    # -- show images --
+    if (len(imgs) == 0):
+        print("Images not found")
+        exit()
+    return imgs
---
+++ QualityLevel.py
@@ -0,0 +1,100 @@
+import ImgLoader as loader
+import cv2
+import numpy as np
+
+img = loader.getImages('imgdirpath')[0]
+sampleMask = loader.getImages('segmentsdirpath')[0]
+
+
+
+ret, th = cv2.threshold(img, 50, 255, 0)
+
+# key = 0
+# thVal = 0
+#
+# while (key != 27): # esc/enter
+#     key = cv2.waitKey()
+#
+#     if (key == 82):
+#         # up
+#         thVal = thVal + 10
+#
+#     elif (key == 84):
+#         # down
+#         thVal = thVal - 10
+#
+#     ret, th = cv2.threshold(img, thVal, 255, cv2.THRESH_BINARY)
+#     cv2.imshow("fresult", th)
+
+def minBTrackCallBack(pos):
+    global minB
+    minB = pos
+    colorSelect()
+
+def maxBTrackCallBack(pos):
+    global maxB
+    maxB = pos
+    colorSelect()
+
+def minGTrackCallBack(pos):
+    global minG
+    minG = pos
+    colorSelect()
+
+def maxGTrackCallBack(pos):
+    global maxG
+    maxG = pos
+    colorSelect()
+
+def minRTrackCallBack(pos):
+    global minR
+    minR = pos
+    colorSelect()
+
+def maxRTrackCallBack(pos):
+    global maxR
+    maxR = pos
+    colorSelect()
+
+def colorSelect():
+    global minB, minG, minR, maxB, maxG, maxR, mask
+    lower_blue = np.array([minB, minG, minR])
+    upper_blue = np.array([maxB, maxG, maxR])
+
+    mask = cv2.inRange(img, lower_blue, upper_blue)
+    res = cv2.bitwise_and(sampleMask, sampleMask, mask=mask)
+
+    cv2.imshow('res', res)
+    cv2.imshow('mask', mask)
+
+
+
+minB = 0
+minG = 0
+minR = 0
+
+maxB = 0
+maxG = 0
+maxR = 0
+
+mask = img.copy()
+
+colorSelect()
+cv2.imshow('frame', img)
+
+cv2.createTrackbar("Blue min", "frame", minB, 255, minBTrackCallBack);
+cv2.createTrackbar("Blue max", "frame", maxB, 255, maxBTrackCallBack);
+
+cv2.createTrackbar("Green min", "frame", minG, 255, minGTrackCallBack);
+cv2.createTrackbar("Green max", "frame", maxG, 255, maxGTrackCallBack);
+
+cv2.createTrackbar("Red min", "frame", minR, 255, minRTrackCallBack);
+cv2.createTrackbar("Red max", "frame", maxR, 255, maxRTrackCallBack);
+
+key = 0
+while (key != 27):
+    key = cv2.waitKey()
+    # if (key == ord('r')):
+    #     cv2.imwrite("/home/ange/Desktop/mask.bmp", mask)
+
+print('------------------')
